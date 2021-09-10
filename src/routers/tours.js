bdab5a50b648dea0d0e485fd25830bde860bf0f9
moinur-rahman <rahmanmoin27@gmail.com>
2021-09-10 22:23:15 +0600

    After fixing link bugs

--- src/routers/tours.js
+++ src/routers/tours.js
@@ -5,6 +5,10 @@ router.get("/tours", (req, res) => {
   res.render("tours");
 });
 
+router.get("/tour-place",(req, res) => {
+  res.render("tour-place");
+})
+
 router.get("/geographical", (req, res) => {
   res.render("HomePage1/GEOGRAPHICAL-PLACES/geographical");
 });

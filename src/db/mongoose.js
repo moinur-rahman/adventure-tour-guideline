e04823fc0689d2f8a68fc58bb029f95ef572298d
moinur-rahman <rahmanmoin27@gmail.com>
2021-09-20 20:19:09 +0600

    Before Hosting Website

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/adventure-tour-guideline", {
+mongoose.connect("mongodb+srv://moinur-rahman:SGmdv8t6iPECeKYQ@cluster0.txj2z.mongodb.net/adventure-tour-guideline?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
